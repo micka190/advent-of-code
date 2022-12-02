ad781a0e30c72ea000844c2cf91f87c7fd171e42
Michael <63615275+micka190@users.noreply.github.com>
2022-12-02 12:10:16 -0700

    Added more tests for solver. Solves every permutation properly.

--- 2022/day 2/Tests/SolverTests.cs
+++ 2022/day 2/Tests/SolverTests.cs
@@ -15,4 +15,27 @@ public class SolverTests
         // Assert
         result.Should().Be(expectedScore);
     }
+
+    [Theory]
+    [InlineData("", 0)] // No  round
+    [InlineData("A X", 1 + 3)] // Rock: 1, Draw: 3
+    [InlineData("B Y", 2 + 3)] // Paper: 2, Draw: 3
+    [InlineData("C Z", 3 + 3)] // Scissors: 3, Draw: 3
+    [InlineData("A Z", 1 + 6)] // Rock: 1, Win: 6
+    [InlineData("B X", 2 + 6)] // Paper: 2, Win: 6
+    [InlineData("C Y", 3 + 6)] // Scissors: 3, Win: 6
+    [InlineData("A Y", 1)] // Rock: 1, Lose: 0
+    [InlineData("B Z", 2)] // Paper: 2, Lose: 0
+    [InlineData("C X", 3)] // Scissors: 3, Lose: 0
+    public void SolveFor_ReturnsExpectedScore_WhenGivenInput(string input, int expectedScore)
+    {
+        // Arrange
+        // ...
+
+        // Act
+        var result = Solver.SolveFor(input);
+
+        // Assert
+        result.Should().Be(expectedScore);
+    }
 }
