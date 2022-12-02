e23812b07df031ccd5045a72a8729202dabd2b4e
Michael <63615275+micka190@users.noreply.github.com>
2022-12-02 12:46:37 -0700

    Started working on Part 2. Added GameRigger to rig games for us.

---
+++ 2022/day 2/Solution/GameRigger.cs
@@ -0,0 +1,32 @@
+﻿namespace Solution;
+
+public static class GameRigger
+{
+    public static Hand Rig(Hand opponent, Outcome desired)
+    {
+        if (desired is Outcome.Draw)
+        {
+            return opponent;
+        }
+        else if (desired is Outcome.Win)
+        {
+            return opponent switch
+            {
+                Hand.Rock => Hand.Paper,
+                Hand.Paper => Hand.Scissors,
+                Hand.Scissors => Hand.Rock,
+                _ => throw new ArgumentOutOfRangeException(nameof(opponent), opponent, "Unsupported hand.")
+            };
+        }
+        else
+        {
+            return opponent switch
+            {
+                Hand.Rock => Hand.Scissors,
+                Hand.Paper => Hand.Rock,
+                Hand.Scissors => Hand.Paper,
+                _ => throw new ArgumentOutOfRangeException(nameof(opponent), opponent, "Unsupported hand.")
+            };
+        }
+    }
+}
---
+++ 2022/day 2/Tests/GameRiggerTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Tests;
+
+public class GameRiggerTests
+{
+    [Theory]
+    [InlineData(Hand.Rock, Outcome.Win, Hand.Paper)]
+    [InlineData(Hand.Rock, Outcome.Lose, Hand.Scissors)]
+    [InlineData(Hand.Rock, Outcome.Draw, Hand.Rock)]
+    [InlineData(Hand.Paper, Outcome.Win, Hand.Scissors)]
+    [InlineData(Hand.Paper, Outcome.Lose, Hand.Rock)]
+    [InlineData(Hand.Paper, Outcome.Draw, Hand.Paper)]
+    [InlineData(Hand.Scissors, Outcome.Win, Hand.Rock)]
+    [InlineData(Hand.Scissors, Outcome.Lose, Hand.Paper)]
+    [InlineData(Hand.Scissors, Outcome.Draw, Hand.Scissors)]
+    public void Rig_ReturnsTheHandNeededToGet_TheDesiredOutcome(Hand opponent, Outcome desired, Hand expectedHand)
+    {
+        // Arrange
+        // ...
+
+        // Act
+        var result = GameRigger.Rig(opponent, desired);
+
+        // Assert
+        result.Should().Be(expectedHand);
+    }
+}
