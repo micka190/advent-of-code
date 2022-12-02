524e3ebdca7832bf200d6f008e26eebf4e2b180c
Michael <63615275+micka190@users.noreply.github.com>
2022-12-02 11:23:43 -0700

    Added scoring to Solver.

--- 2022/day 2/Solution/Solver.cs
+++ 2022/day 2/Solution/Solver.cs
@@ -2,6 +2,20 @@
 
 public static class Solver
 {
+    public static readonly Dictionary<Hand, int> HandScores = new()
+    {
+        {Hand.Rock, 1},
+        {Hand.Paper, 2},
+        {Hand.Scissors, 3},
+    };
+    
+    public static readonly Dictionary<Outcome, int> OutcomeScores = new()
+    {
+        { Outcome.Win, 6},
+        { Outcome.Lose, 0},
+        { Outcome.Draw, 3},
+    };
+
     public static int SolveFor(string input)
     {
         return 0;
