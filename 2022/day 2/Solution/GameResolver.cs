35cfe65bd56133e2a81cfe1cee8834333f544175
Michael <63615275+micka190@users.noreply.github.com>
2022-12-02 11:58:53 -0700

    Added GameResolver and tests. Allows us to get the outcome of a game of rock paper scissors.

---
+++ 2022/day 2/Solution/GameResolver.cs
@@ -0,0 +1,23 @@
+﻿namespace Solution;
+
+public static class GameResolver
+{
+    public static Outcome Resolve(Hand player, Hand opponent)
+    {
+        if (player == opponent)
+        {
+            return Outcome.Draw;
+        }
+
+        if (
+            (player == Hand.Rock && opponent == Hand.Scissors) ||
+            (player == Hand.Paper && opponent == Hand.Rock) ||
+            (player == Hand.Scissors && opponent == Hand.Paper)
+            )
+        {
+            return Outcome.Win;
+        }
+
+        return Outcome.Lose;
+    }
+}
---
+++ 2022/day 2/Tests/GameResolverTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Tests;
+
+public class GameResolverTests
+{
+    [Theory]
+    [InlineData(Hand.Rock, Hand.Rock, Outcome.Draw)]
+    [InlineData(Hand.Rock, Hand.Paper, Outcome.Lose)]
+    [InlineData(Hand.Rock, Hand.Scissors, Outcome.Win)]
+    [InlineData(Hand.Paper, Hand.Rock, Outcome.Win)]
+    [InlineData(Hand.Paper, Hand.Paper, Outcome.Draw)]
+    [InlineData(Hand.Paper, Hand.Scissors, Outcome.Lose)]
+    [InlineData(Hand.Scissors, Hand.Rock, Outcome.Lose)]
+    [InlineData(Hand.Scissors, Hand.Paper, Outcome.Win)]
+    [InlineData(Hand.Scissors, Hand.Scissors, Outcome.Draw)]
+    public void Resolve_ReturnsOutcome_OfGivenHandCombination(Hand player, Hand opponent, Outcome expected)
+    {
+        // Arrange
+        // ...
+
+        // Act
+        var outcome = GameResolver.Resolve(player, opponent);
+
+        // Assert
+        outcome.Should().Be(expected);
+    }
+}
