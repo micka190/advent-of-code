ac0bb33b69d44673c45b7021fd8b078af6cb24ea
Michael <63615275+micka190@users.noreply.github.com>
2022-12-02 11:50:49 -0700

    Added StrategyParser and tests. It parses a given line of the strategy guide and returns the right hands.

---
+++ 2022/day 2/Solution/StrategyParser.cs
@@ -0,0 +1,37 @@
+﻿namespace Solution;
+
+public static class StrategyParser
+{
+    public static readonly Dictionary<string, Hand> Hands = new()
+    {
+        { "A", Hand.Rock },
+        { "B", Hand.Paper },
+        { "C", Hand.Scissors },
+        
+        { "X", Hand.Rock },
+        { "Y", Hand.Paper },
+        { "Z", Hand.Scissors },
+    };
+
+    public static (Hand Left, Hand Right) ParseLine(string input)
+    {
+        var tokens = input.Trim().Split(" ");
+        
+        if (tokens.Length != 2)
+        {
+            throw new ArgumentException(
+                $"Invalid input provided to strategy parser. Expected 2 letters with a space between. Got \"{input}\"."
+            );
+        }
+        else if (!Hands.ContainsKey(tokens[0]))
+        {
+            throw new ArgumentOutOfRangeException(nameof(input), "Left character is unsupported.");
+        }
+        else if (!Hands.ContainsKey(tokens[1]))
+        {
+            throw new ArgumentOutOfRangeException(nameof(input), "Right character is unsupported.");
+        }
+        
+        return (Left: Hands[tokens[0]], Right: Hands[tokens[1]]);
+    }
+}
---
+++ 2022/day 2/Tests/StrategyParserTests.cs
@@ -0,0 +1,49 @@
+﻿namespace Tests;
+
+public class StrategyParserTests
+{
+    [Theory]
+    [InlineData("A X", Hand.Rock, Hand.Rock)]
+    [InlineData("B Y", Hand.Paper, Hand.Paper)]
+    [InlineData("C Z", Hand.Scissors, Hand.Scissors)]
+    public void ParseLine_ReturnsTupleOfHands_GivenValidInput(string input, Hand expectedLeft, Hand expectedRight)
+    {
+        // Arrange
+        // ...
+
+        // Act
+        var (left, right) = StrategyParser.ParseLine(input);
+
+        // Assert
+        left.Should().Be(expectedLeft);
+        right.Should().Be(expectedRight);
+    }
+
+    [Fact]
+    public void ParseLine_ThrowsArgumentException_WhenGivenInvalidInput()
+    {
+        // Arrange
+        const string invalidInput = "A B ERROR\n ";
+
+        // Act
+        var act = () => StrategyParser.ParseLine(invalidInput);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
+
+    [Theory]
+    [InlineData("_ A", "Left character is unsupported.")]
+    [InlineData("A _", "Right character is unsupported.")]
+    public void ParseLine_ThrowsArgumentOutOfRangeException_WhenGivenLettersItDoesNotKnow(string input, string errorMessage)
+    {
+        // Arrange
+        var wildcardMessagePattern = $"*{errorMessage}*";
+
+        // Act
+        var act = () => StrategyParser.ParseLine(input);
+
+        // Assert
+        act.Should().Throw<ArgumentOutOfRangeException>().WithMessage(wildcardMessagePattern);
+    }
+}
