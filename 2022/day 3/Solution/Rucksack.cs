1e9034c36a8eb04dbebc8522ad9af2ca7813513f
Michael <63615275+micka190@users.noreply.github.com>
2022-12-03 14:21:27 -0700

    Added Rucksack class, and tests. Able to split a string input in two compartments.

---
+++ 2022/day 3/Solution/Rucksack.cs
@@ -0,0 +1,14 @@
+﻿namespace Solution;
+
+public class Rucksack
+{
+    public List<string> Items { get; }
+
+    public IEnumerable<string> FirstCompartment => Items.GetRange(0, Items.Count / 2);
+    public IEnumerable<string> SecondCompartment => Items.GetRange(Items.Count / 2, Items.Count / 2);
+
+    public Rucksack(List<string> items)
+    {
+        Items = items;
+    }
+}
---
+++ 2022/day 3/Tests/RucksackTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Tests;
+
+public class RucksackTests
+{
+    [Fact]
+    public void FirstCompartment_ReturnsFirstHalf_OfItems()
+    {
+        // Arrange
+        var items = new List<string>{"A", "B", "C", "1", "2", "3"};
+        var rucksack = new Rucksack(items);
+
+        // Act
+        var compartment = rucksack.FirstCompartment;
+
+        // Assert
+        compartment.Should().BeEquivalentTo(new List<string>{"A", "B", "C"});
+    }
+
+    [Fact]
+    public void SecondCompartment_ReturnsSecondHalf_OfItems()
+    {
+        // Arrange
+        var items = new List<string>{"A", "B", "C", "1", "2", "3"};
+        var rucksack = new Rucksack(items);
+
+        // Act
+        var compartment = rucksack.SecondCompartment;
+
+        // Assert
+        compartment.Should().BeEquivalentTo(new List<string>{"1", "2", "3"});
+    }
+}
