eb80346b8adf57e141e1f4040cd209eea16d5da3
Michael <63615275+micka190@users.noreply.github.com>
2022-12-03 15:18:37 -0700

    Added Solver class. Can solve part 1 example on Advent of Code website.

---
+++ 2022/day 3/Solution/Solver.cs
@@ -0,0 +1,29 @@
+﻿namespace Solution;
+
+public static class Solver<TPriorityProvider> 
+    where TPriorityProvider : IPriorityProvider, new()
+{
+    public static int SolveForPartOne(string input) =>
+        string.IsNullOrEmpty(input)
+            ? 0
+            : input
+                .Trim()
+                .Split('\n')
+                .Select(line => new Rucksack(line))
+                .Sum(ComputeRucksackPriority);
+
+    private static int ComputeRucksackPriority(Rucksack rucksack)
+    {
+        var priorityProvider = new TPriorityProvider();
+        var overlappingItemTypes = rucksack.OverlappingItemTypes.ToList();
+        
+        if (overlappingItemTypes.Count != 1)
+        {
+            throw new InvalidOperationException(
+                $"Rucksack has unexpected amount of overlapping item types. Expected 1, got {overlappingItemTypes.Count}"
+            );
+        }
+
+        return priorityProvider.GetPriority(overlappingItemTypes[0]);
+    }
+}
---
+++ 2022/day 3/Tests/SolverTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests;
+
+public class SolverTests
+{
+    [Fact]
+    public void SolveForPartOne_ReturnsCorrectSum_ForAdventOfCodeWebsiteDemo()
+    {
+        // Arrange
+        const string input = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw\n";
+        const int expectedSum = 157;
+
+        // Act
+        var result = Solver<PriorityProvider>.SolveForPartOne(input);
+
+        // Assert
+        result.Should().Be(expectedSum);
+    }
+
+    [Theory]
+    [InlineData("aa\naa\naa", 3)] // a = 1, *3, total: 3
+    [InlineData("abbc\nXX\n", 52)] // b = 2, X = 50, total: 52
+    [InlineData("CC\nBcqefeabgp\nabcd123a", 35)] // C = 29, e = 5, a = 1, total: 35
+    public void SolveForPartOne_ReturnsExpectedSum_ForGivenInput(string input, int expectedSum)
+    {
+        // Arrange
+        // ...
+
+        // Act
+        var result = Solver<PriorityProvider>.SolveForPartOne(input);
+
+        // Assert
+        result.Should().Be(expectedSum);
+    }
+}
