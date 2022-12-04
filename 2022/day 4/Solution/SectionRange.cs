35a2cf0838edec04528e73d60b83c36d020a06f8
Michael <63615275+micka190@users.noreply.github.com>
2022-12-04 11:45:33 -0700

    Added SectionRange class. Can parse a given range string and give us the start and end.

---
+++ 2022/day 4/Solution/SectionRange.cs
@@ -0,0 +1,20 @@
+﻿namespace Solution;
+
+public class SectionRange
+{
+    public int Start { get; set; }
+    public int End { get; set; }
+
+    public SectionRange(string range)
+    {
+        var segments = range.Split('-');
+        
+        if (segments.Length != 2)
+        {
+            throw new FormatException($"Invalid range input. Expected 2 numbers separated by a dash. Got: \"{range}\"");
+        }
+        
+        Start = int.Parse(segments[0]);
+        End = int.Parse(segments[1]);
+    }
+}
---
+++ 2022/day 4/Tests/SectionRangeTests.cs
@@ -0,0 +1,46 @@
+﻿namespace Tests;
+
+public class SectionRangeTests
+{
+    [Fact]
+    public void Constructor_ThrowsFormatException_WhenGivenInvalidInputFormat()
+    {
+        // Arrange
+        const string invalidInput = "123-456-789-0";
+
+        // Act
+        var act = () => new SectionRange(invalidInput);
+
+        // Assert
+        act.Should().Throw<FormatException>();
+    }
+
+    [Fact]
+    public void Constructor_ThrowsFormatException_WhenGivenInputWithoutNumbers()
+    {
+        // Arrange
+        const string inputWithoutNumbers = "abc-def";
+
+        // Act
+        var act = () => new SectionRange(inputWithoutNumbers);
+
+        // Assert
+        act.Should().Throw<FormatException>();
+    }
+
+    [Fact]
+    public void Constructor_ParsesSectionIdNumbers_WhenGivenValidInput()
+    {
+        // Arrange
+        const int start = 2;
+        const int end = 8;
+        var range = $"{start}-{end}";
+
+        // Act
+        var sectionRange = new SectionRange(range);
+
+        // Assert
+        sectionRange.Start.Should().Be(start);
+        sectionRange.End.Should().Be(end);
+    }
+}
