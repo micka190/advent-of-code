3a15406e777bc948c043f7940eade7b289e3d857
Michael <63615275+micka190@users.noreply.github.com>
2022-12-04 12:06:23 -0700

    Added Solver class (and tests). It works with the example input from the AoC website, and returns the right answer to Part 1.

---
+++ 2022/day 4/Solution/Solver.cs
@@ -0,0 +1,26 @@
+﻿namespace Solution;
+
+public static class Solver
+{
+    public static int SolveForPartOne(string input) =>
+        string.IsNullOrEmpty(input)
+            ? 0
+            : input
+                .Trim()
+                .Split('\n')
+                .Select(ComputeOverlappingSections)
+                .Count(overlapping => overlapping);
+
+    private static bool ComputeOverlappingSections(string line)
+    {
+        var segments = line.Split(',');
+        if (segments.Length != 2)
+        {
+            throw new FormatException($"Invalid line format. Expected 2 ranges separated by a comma. Got: \"{line}\"");
+        }
+
+        var first = new SectionRange(segments[0]);
+        var second = new SectionRange(segments[1]);
+        return first.Contains(second) || second.Contains(first);
+    }
+}
---
+++ 2022/day 4/Tests/SolverTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Tests;
+
+public class SolverTests
+{
+    [Fact]
+    public void SolveForPartOne_ReturnsNumberOfOverlappingAssignments_GivenAdventOfCodeExampleInput()
+    {
+        // Arrange
+        const string input = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8\n";
+        const int expectedCount = 2;
+
+        // Act
+        var result = Solver.SolveForPartOne(input);
+
+        // Assert
+        result.Should().Be(expectedCount);
+    }
+
+    [Theory]
+    [InlineData("", 0)]
+    [InlineData("1-4,5-9\n0-3,5-8", 0)]
+    [InlineData("0-9,0-9\n1-2,3-5\n0-6,5-6\n", 2)]
+    [InlineData("3-5,3-4\n1-6,4-6\n0-9,9-10", 2)]
+    public void SolveForPartOne_ReturnsExpectedNumberOfOverlappingAssignments_GivenValidInput(string input, int expectedCount)
+    {
+        // Arrange
+        // ...
+
+        // Act
+        var result = Solver.SolveForPartOne(input);
+
+        // Assert
+        result.Should().Be(expectedCount);
+    }
+}
