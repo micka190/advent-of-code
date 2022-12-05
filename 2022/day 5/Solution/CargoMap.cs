358892f55921a490b25883448be810d029f84244
Michael <63615275+micka190@users.noreply.github.com>
2022-12-05 12:38:17 -0700

    Implemented CargoMap.Move() method. It works for single and multiple crates.

--- 2022/day 5/Solution/CargoMap.cs
+++ 2022/day 5/Solution/CargoMap.cs
@@ -35,6 +35,14 @@ public class CargoMap
 
     public void Move(int count, int from, int to)
     {
-        throw new NotImplementedException();
+        // Subtract 1 from "from" and "to", because the problem uses a 1-based index system, but we use a 0-based index system.
+        var startIndex = from - 1;
+        var endIndex = to - 1;
+
+        for (var i = 0; i < count; ++i)
+        {
+            var crate = Stacks[startIndex].Pop();
+            Stacks[endIndex].Push(crate);
+        }
     }
 }
--- 2022/day 5/Tests/CargoMapTests.cs
+++ 2022/day 5/Tests/CargoMapTests.cs
@@ -6,7 +6,9 @@ public class CargoMapTests
     public void Constructor_Parses_StringRepresentation()
     {
         // Arrange
-        const string representation = "[x] [y] [z]\n[a] [b] [c]\n 1   2   3 ";
+        const string representation = "[x] [y] [z]\n" + 
+                                      "[a] [b] [c]\n" + 
+                                      " 1   2   3 ";
 
         // Act
         var map = new CargoMap(representation);
@@ -20,7 +22,10 @@ public class CargoMapTests
     public void Constructor_Parses_UnevenStringRepresentation()
     {
         // Arrange
-        const string representation = "[1]        \n[x]     [z]\n[a] [b] [c]\n 1   2   3 ";
+        const string representation = "[1]        \n" +
+                                      "[x]     [z]\n" +
+                                      "[a] [b] [c]\n" +
+                                      " 1   2   3 ";
 
         // Act
         var map = new CargoMap(representation);
@@ -28,5 +33,45 @@ public class CargoMapTests
         // Assert
         map.Stacks.Count.Should().Be(3);
         map.TopMostCrates.Should().BeEquivalentTo(new List<char> { '1', 'b', 'z' });
+    }
+
+    [Fact]
+    public void Move_MovesACrate_FromOneStackToAnother()
+    {
+        // Arrange
+        const string representation = "[1]        \n" +
+                                      "[x]     [z]\n" +
+                                      "[a] [b] [c]\n" +
+                                      " 1   2   3 ";
+        var map = new CargoMap(representation);
+
+        // Act
+        map.Move(1, 1, 2);
+
+        // Assert
+        map.Stacks.Count.Should().Be(3);
+        map.TopMostCrates.Should().BeEquivalentTo(new List<char> { 'x', '1', 'z' });
+    }
+
+    [Fact]
+    public void Move_MovesMultipleCrates_FromOneStackToAnother()
+    {
+        // Arrange
+        const string representation = "[1]        \n" +
+                                      "[x]     [z]\n" +
+                                      "[a] [b] [c]\n" +
+                                      " 1   2   3 ";
+
+        var map = new CargoMap(representation);
+
+        // Act
+        map.Move(2, 1, 3);
+
+        // Assert
+        map.Stacks.Count.Should().Be(3);
+        map.TopMostCrates.Should().BeEquivalentTo(new List<char> { 'a', 'b', 'x' });
+        map.Stacks[0].Should().Equal('a');
+        map.Stacks[1].Should().Equal('b');
+        map.Stacks[2].Should().Equal('x', '1', 'z', 'c');
     }
 }
