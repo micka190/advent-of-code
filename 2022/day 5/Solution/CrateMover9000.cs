a69a45b802729c8f29594be6b93c56042f6fbdc1
Michael <63615275+micka190@users.noreply.github.com>
2022-12-05 13:36:36 -0700

    Split CrateMover9000 logic into a BaseCrateMover class, since we're only changing the Move method for Part 2.

--- 2022/day 5/Solution/CrateMover9000.cs
+++ 2022/day 5/Solution/CrateMover9000.cs
@@ -1,39 +1,12 @@
 ﻿namespace Solution;
 
-public class CrateMover9000
+public class CrateMover9000 : BaseCrateMover
 {
-    public List<Stack<char>> Stacks { get; } = new();
-    public IEnumerable<char> TopMostCrates => Stacks.Select(stack => stack.Peek());
-
-    public CrateMover9000(string representation)
+    public CrateMover9000(string representation) : base(representation)
     {
-        var lines = representation.Split('\n');
-        var lastLine = lines.Last().Trim();
-        var lastLineNumbers = lastLine.Split(' ');
-        var count = int.Parse(lastLineNumbers.Last());
-
-        for (var i = 0; i < count; ++i)
-        {
-            Stacks.Add(new Stack<char>());
-        }
-
-        const int startIndex = 1;
-        const int gapBetweenCharacters = 4;
-        foreach (var line in lines[..^1].Reverse())
-        {
-            for (var col = 0; col < count; ++col)
-            {
-                var index = startIndex + col * gapBetweenCharacters;
-                var character = line[index];
-                if (character != ' ')
-                {
-                    Stacks[col].Push(character);
-                }
-            }
-        }
     }
 
-    public void Move(int count, int from, int to)
+    public override void Move(int count, int from, int to)
     {
         // Subtract 1 from "from" and "to", because the problem uses a 1-based index system, but we use a 0-based index system.
         var startIndex = from - 1;
--- 2022/day 5/Solution/Solver.cs
+++ 2022/day 5/Solution/Solver.cs
@@ -5,14 +5,14 @@ public static class Solver
     public static IEnumerable<char> SolveForPartOne(string input)
     {
         var inputParser = new InputParser(input);
-        var cargoMap = new CrateMover9000(inputParser.CargoMapRepresentation);
+        var crateMover = new CrateMover9000(inputParser.CargoMapRepresentation);
         var instructionReader = new InstructionReader(inputParser.Instructions);
 
         foreach (var instruction in instructionReader.ReadInstructions())
         {
-            cargoMap.Move(instruction.Count, instruction.From, instruction.To);
+            crateMover.Move(instruction.Count, instruction.From, instruction.To);
         }
 
-        return cargoMap.TopMostCrates;
+        return crateMover.TopMostCrates;
     }
 }
