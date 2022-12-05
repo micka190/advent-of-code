643a23728b7dd7b28d6b75f025af14ade7674e21
Michael <63615275+micka190@users.noreply.github.com>
2022-12-05 13:51:19 -0700

    Revert "Renamed CargoMap to CrateMover9000 in preparation for Part 2 code change."
    This reverts commit 8e3c0ddee6490dd9c79de61c5d9f9aa283cb1314.

--- 2022/day 5/Solution/Solver.cs
+++ 2022/day 5/Solution/Solver.cs
@@ -5,7 +5,7 @@ public static class Solver
     public static IEnumerable<char> SolveForPartOne(string input)
     {
         var inputParser = new InputParser(input);
-        var cargoMap = new CrateMover9000(inputParser.CargoMapRepresentation);
+        var cargoMap = new CargoMap(inputParser.CargoMapRepresentation);
         var instructionReader = new InstructionReader(inputParser.Instructions);
 
         foreach (var instruction in instructionReader.ReadInstructions())
