a22556910b4eb50f66f71f41654ee36226feef71
Michael <63615275+micka190@users.noreply.github.com>
2022-12-05 11:42:39 -0700

    Added CargoMap class and tests. It takes a cargo map string representation, and turns it into a list of stacks. It will also let us move crates around (not implemented yet).

---
+++ 2022/day 5/Solution/CargoMap.cs
@@ -0,0 +1,40 @@
+﻿namespace Solution;
+
+public class CargoMap
+{
+    public List<Stack<char>> Stacks { get; } = new();
+    public List<char> TopMostCrates => Stacks.Select(stack => stack.Peek()).ToList();
+
+    public CargoMap(string representation)
+    {
+        var lines = representation.Split('\n');
+        var lastLine = lines.Last().Trim();
+        var lastLineNumbers = lastLine.Split(' ');
+        var count = int.Parse(lastLineNumbers.Last());
+
+        for (var i = 0; i < count; ++i)
+        {
+            Stacks.Add(new Stack<char>());
+        }
+
+        const int startIndex = 1;
+        const int gapBetweenCharacters = 4;
+        foreach (var line in lines[..^1].Reverse())
+        {
+            for (var col = 0; col < count; ++col)
+            {
+                var index = startIndex + col * gapBetweenCharacters;
+                var character = line[index];
+                if (character != ' ')
+                {
+                    Stacks[col].Push(character);
+                }
+            }
+        }
+    }
+
+    public void Move(int count, int from, int to)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ 2022/day 5/Tests/CargoMapTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Tests;
+
+public class CargoMapTests
+{
+    [Fact]
+    public void Constructor_Parses_StringRepresentation()
+    {
+        // Arrange
+        const string representation = "[x] [y] [z]\n[a] [b] [c]\n 1   2   3 ";
+
+        // Act
+        var map = new CargoMap(representation);
+
+        // Assert
+        map.Stacks.Count.Should().Be(3);
+        map.TopMostCrates.Should().BeEquivalentTo(new List<char> { 'x', 'y', 'z' });
+    }
+
+    [Fact]
+    public void Constructor_Parses_UnevenStringRepresentation()
+    {
+        // Arrange
+        const string representation = "[1]        \n[x]     [z]\n[a] [b] [c]\n 1   2   3 ";
+
+        // Act
+        var map = new CargoMap(representation);
+
+        // Assert
+        map.Stacks.Count.Should().Be(3);
+        map.TopMostCrates.Should().BeEquivalentTo(new List<char> { '1', 'b', 'z' });
+    }
+}
