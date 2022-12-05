9f5f3c74b357a8a5d33135715e99c796db284ce1
Michael <63615275+micka190@users.noreply.github.com>
2022-12-05 11:13:48 -0700

    Added InputParser class that splits the given input into the cargo map and the instructions (also tests).

---
+++ 2022/day 5/Solution/InputParser.cs
@@ -0,0 +1,19 @@
+﻿namespace Solution;
+
+public class InputParser
+{
+    public readonly string CargoMap;
+    public readonly string Instructions;
+    
+    public InputParser(string input)
+    {
+        var segments = input.Split("\n\n");
+        if (segments.Length != 2)
+        {
+            throw new FormatException($"Invalid input. Expected map and instructions to be divided by an empty line. Got {input}");
+        }
+
+        CargoMap = segments[0];
+        Instructions = segments[1];
+    }
+}
---
+++ 2022/day 5/Tests/InputParserTests.cs
@@ -0,0 +1,20 @@
+﻿namespace Tests;
+
+public class InputParserTests
+{
+    [Fact]
+    public void Constructor_SplitsInputIntoMapAndInstructions_WhenGivenValidInput()
+    {
+        // Arrange
+        const string cargoMap = "[a] [b] [c]\n 1   2   3";
+        const string instructions = "move 1 from 1 to 2\nmove 2 from 2 to 3\n";
+        const string input = $"{cargoMap}\n\n{instructions}";
+
+        // Act
+        var parser = new InputParser(input);
+
+        // Assert
+        parser.CargoMap.Should().Be(cargoMap);
+        parser.Instructions.Should().Be(instructions);
+    }
+}
