e812a4d7af0783ad23e29354de1223bd107f0a87
Michael <63615275+micka190@users.noreply.github.com>
2022-12-06 13:18:20 -0700

    Added `Solver` class, which is just a thin wrapper around `SignalParser`. We don't need to write tests for it, since it basically just calls `SignalParser.FindStart()` (which is already tested).

--- 2022/day 6/Solution/Program.cs
+++ 2022/day 6/Solution/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Solution;
 
-Console.WriteLine("Hello, World!");
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Error: Input file path not passed.");
+        }
+        else if (File.Exists(args[0]))
+        {
+            var input = File.ReadAllText(args[0]);
+            var solver = new Solver();
+            
+            Console.WriteLine($"PART 1 - {solver.SolveForPartOne(input)}");
+        }
+        else
+        {
+            Console.WriteLine($"Error: Could not open file {args[0]}");
+        }
+    }
+}
---
+++ 2022/day 6/Solution/Solver.cs
@@ -0,0 +1,11 @@
+﻿namespace Solution;
+
+public class Solver
+{
+    public int SolveForPartOne(string input)
+    {
+        const int chunkSize = 4;
+        var parser = new SignalParser();
+        return parser.FindStart(input, chunkSize);
+    }
+}
