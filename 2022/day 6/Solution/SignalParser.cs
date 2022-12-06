4b2c9a228d229f376241b0033d0c9e9f86edc8cd
Michael <63615275+micka190@users.noreply.github.com>
2022-12-06 13:13:38 -0700

    Implemented `SignalParser.FindStart(string input, int chunkSize)` to find the first unique chunk of size N in a given input. Added tests as well.

--- 2022/day 6/Solution/SignalParser.cs
+++ 2022/day 6/Solution/SignalParser.cs
@@ -2,5 +2,23 @@
 
 public class SignalParser
 {
-    
+    public int FindStart(string input, int chunkSize)
+    {
+        if (chunkSize > input.Length)
+        {
+            throw new ArgumentException("Chunk size must be smaller than total input length.");
+        }
+        
+        for (var i = 0; i < input.Length - chunkSize; ++i)
+        {
+            var chunk = input.Substring(i, chunkSize);
+            var set = chunk.ToHashSet();
+            if (set.Count == chunkSize)
+            {
+                return i + chunkSize;
+            }
+        }
+
+        return -1;
+    }
 }
--- 2022/day 6/Tests/SignalParserTests.cs
+++ 2022/day 6/Tests/SignalParserTests.cs
@@ -2,5 +2,52 @@
 
 public class SignalParserTests
 {
-    
+    [Theory]
+    [InlineData("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 7)]
+    [InlineData("bvwbjplbgvbhsrlpgdmjqwftvncz", 5)]
+    [InlineData("nppdvjthqldpwncqszvftbrmjlhg", 6)]
+    [InlineData("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10)]
+    [InlineData("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)]
+    public void FindStart_ReturnsCorrectValue_GivenAdventOfCodeExampleInput(string input, int expectedStart)
+    {
+        // Arrange
+        const int chunkSize = 4;
+        var parser = new SignalParser();
+
+        // Act
+        var result = parser.FindStart(input, chunkSize);
+
+        // Assert
+        result.Should().Be(expectedStart);
+    }
+
+    [Fact]
+    public void FindStart_ReturnsMinusOne_WhenNoUniqueChunkIsFound()
+    {
+        // Arrange
+        const string input = "aaaaaaaaaaaa";
+        const int chunkSize = 4;
+        var parser = new SignalParser();
+
+        // Act
+        var result = parser.FindStart(input, chunkSize);
+
+        // Assert
+        result.Should().Be(-1);
+    }
+
+    [Fact]
+    public void FindStart_ThrowsArgumentException_GivenInputSmallerThanChunkSize()
+    {
+        // Arrange
+        const string input = "abc";
+        var chunkSize = input.Length * 2;
+        var parser = new SignalParser();
+
+        // Act
+        var act = () => parser.FindStart(input, chunkSize);
+
+        // Assert
+        act.Should().Throw<ArgumentException>();
+    }
 }
