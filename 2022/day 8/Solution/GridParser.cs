f6f0ed031503c9c881af7cfa60d4bf3c07a4f56f
Michael <63615275+micka190@users.noreply.github.com>
2022-12-08 12:15:38 -0700

    Added `GridParser` and tests. Parses the input and converts it into a 2d array of numbers.

---
+++ 2022/day 8/Solution/GridParser.cs
@@ -0,0 +1,29 @@
+﻿namespace Solution;
+
+public class GridParser
+{
+    public int[,] Parse(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            throw new ArgumentNullException(nameof(input), "Input must not be null or empty.");
+        }
+
+        var lines = input.Trim().Split('\n');
+        
+        var height = lines.Length;
+        var width = lines[0].Length;
+
+        var grid = new int[height, width];
+        
+        for (var col = 0; col < width; ++col)
+        {
+            for (var row = 0; row < height; ++row)
+            {
+                grid[row, col] = int.Parse(lines[row][col].ToString());
+            }
+        }
+
+        return grid;
+    }
+}
---
+++ 2022/day 8/Tests/GridParserTests.cs
@@ -0,0 +1,32 @@
+﻿namespace Tests;
+
+public class GridParserTests
+{
+    [Fact]
+    public void Parse_ReturnsCorrectGrid_GivenAdventOfCodeExampleInput()
+    {
+        // Arrange
+        const string input = "30373\n" +
+                             "25512\n" +
+                             "65332\n" +
+                             "33549\n" +
+                             "35390\n";
+
+        var expectedGrid = new[,]
+        {
+            {3, 0, 3, 7, 3},
+            {2, 5, 5, 1, 2},
+            {6, 5, 3, 3, 2},
+            {3, 3, 5, 4, 9},
+            {3, 5, 3, 9, 0}
+        };
+
+        var parser = new GridParser();
+
+        // Act
+        var results = parser.Parse(input);
+
+        // Assert
+        results.Should().BeEquivalentTo(expectedGrid);
+    }
+}
