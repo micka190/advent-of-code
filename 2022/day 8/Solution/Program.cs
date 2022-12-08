46abbcc8d49a4d8ebeaaa854fee1b38ac6ce3d33
Michael <63615275+micka190@users.noreply.github.com>
2022-12-08 10:53:04 -0700

    Initial commit for day 8.

---
+++ 2022/day 8/Solution/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Solution;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Error: Input file path not passed.");
+        }
+        else if (File.Exists(args[0]))
+        {
+            var input = File.ReadAllText(args[0]);
+            // TODO: Implement solution.
+        }
+        else
+        {
+            Console.WriteLine($"Error: Could not open file {args[0]}");
+        }
+    }
+}
