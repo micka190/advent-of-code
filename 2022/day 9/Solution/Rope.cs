f70447a6c7516c9df85eb6cd5cc1328ea35a7708
Michael <63615275+micka190@users.noreply.github.com>
2022-12-09 13:16:43 -0700

    Added `Rope` class and tests. `Rope` simulates how Head and Tail move according to the AoC Day 9 specifications.

---
+++ 2022/day 9/Solution/Rope.cs
@@ -0,0 +1,84 @@
+﻿using System.Drawing;
+
+namespace Solution;
+
+public class Rope
+{
+    private Point Head { get; set; }
+    private Point Tail { get; set; }
+
+    public readonly HashSet<Point> UniqueTailPositions;
+
+    public Rope()
+    {
+        Head = new Point();
+        Tail = new Point();
+        UniqueTailPositions = new HashSet<Point> { Tail };
+    }
+
+    public void PerformMotion(Motion motion)
+    {
+        for (var i = 0; i < motion.Steps; ++i)
+        {
+            Head = Move(Head, motion.Direction);
+
+            if (!KnotsTouching())
+            {
+                FollowHeadWithTail(motion.Direction);
+            }
+        }
+    }
+
+    private static Point Move(Point original, Direction direction) =>
+        direction switch
+        {
+            Direction.Up => original with { Y = original.Y + 1 },
+            Direction.Down => original with { Y = original.Y - 1 },
+            Direction.Left => original with { X = original.X - 1 },
+            Direction.Right => original with { X = original.X + 1 },
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, "Unsupported direction")
+        };
+
+    private bool KnotsTouching()
+    {
+        // Overlapping counts as touching.
+        var overlapping = Head == Tail;
+
+        var deltaX = Math.Abs(Head.X - Tail.X);
+        var deltaY = Math.Abs(Head.Y - Tail.Y);
+
+        var touchingX = deltaX == 1 && deltaY == 0;
+        var touchingY = deltaY == 1 && deltaX == 0;
+        var touchingDiagonally = deltaX == 1 && deltaY == 1;
+
+        return overlapping || touchingX || touchingY || touchingDiagonally;
+    }
+
+    private void FollowHeadWithTail(Direction lastHeadDirection)
+    {
+        // This method is only called when Head and Tail are NOT touching.
+        // If BOTH axis are not equal, then Head and Tail were touching diagonally, and Head has just moved away from Tail.
+        // When this happens, Tail must move diagonally towards Head (they end up sharing the same X or Y value afterwards).
+        
+        var requiresDiagonalMovement = Head.X != Tail.X && Head.Y != Tail.Y;
+
+        var headMovedHorizontally = lastHeadDirection is Direction.Right or Direction.Left;
+        var headMovedVertically = lastHeadDirection is Direction.Up or Direction.Down;
+
+        Tail = Move(Tail, lastHeadDirection);
+
+        if (requiresDiagonalMovement)
+        {
+            if (headMovedHorizontally)
+            {
+                Tail = Tail with { Y = Head.Y };
+            }
+            else if (headMovedVertically)
+            {
+                Tail = Tail with { X = Head.X };
+            }
+        }
+
+        UniqueTailPositions.Add(Tail);
+    }
+}
---
+++ 2022/day 9/Tests/RopeTests.cs
@@ -0,0 +1,33 @@
+﻿namespace Tests;
+
+public class RopeTests
+{
+    [Fact]
+    public void PerformMotion_UpdatesTailPositions_GivenAdventOfCodeExampleInput()
+    {
+        // Arrange
+        var input = new List<Motion>
+        {
+            new(Direction.Right, 4),
+            new(Direction.Up, 4),
+            new(Direction.Left, 3),
+            new(Direction.Down, 1),
+            new(Direction.Right, 4),
+            new(Direction.Down, 1),
+            new(Direction.Left, 5),
+            new(Direction.Right, 2),
+        };
+        const int expectedPositions = 13;
+
+        var rope = new Rope();
+
+        // Act
+        foreach (var motion in input)
+        {
+            rope.PerformMotion(motion);
+        }
+
+        // Assert
+        rope.UniqueTailPositions.Count.Should().Be(expectedPositions);
+    }
+}
