bca60c8505409187c59b5fed8ffa0a0dc57765c2
Michael <63615275+micka190@users.noreply.github.com>
2022-12-10 12:29:50 -0700

    Added `Cpu` class and tests, as well as `Register` enum (in case Part 2 adds more registers).
    `Cpu` can currently "tick", which tracks the register value of each cycle.

---
+++ 2022/day 10/Solution/Cpu.cs
@@ -0,0 +1,24 @@
+﻿namespace Solution;
+
+public class Cpu
+{
+    public const int StartingX = 1;
+
+    public readonly Dictionary<Register, int> Registers = new()
+    {
+        { Register.X, StartingX },
+    };
+
+    public readonly Dictionary<Register, List<int>> Cycles = new()
+    {
+        { Register.X, new List<int> { StartingX } }
+    };
+
+    public void Tick()
+    {
+        foreach (var (register, cycle) in Cycles)
+        {
+            cycle.Add(Registers[register]);
+        }
+    }
+}
---
+++ 2022/day 10/Tests/CpuTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Tests;
+
+public class CpuTests
+{
+    [Fact]
+    public void Tick_UpdatedCyclesInformation_WhenCalled()
+    {
+        // Arrange
+        const int expectedValue = Cpu.StartingX;
+        var cpu = new Cpu();
+
+        // Act
+        var originalX = cpu.Registers[Register.X];
+        cpu.Tick();
+        var firstTickX = cpu.Cycles[Register.X][1];
+        cpu.Tick();
+        var secondTickX = cpu.Cycles[Register.X][2];
+
+        // Assert
+        originalX.Should().Be(expectedValue);
+        firstTickX.Should().Be(expectedValue);
+        secondTickX.Should().Be(expectedValue);
+
+        cpu.Cycles[Register.X].Should().HaveCount(3);
+    }
+}
