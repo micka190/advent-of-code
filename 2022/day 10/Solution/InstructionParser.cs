f1a3ca4a0a5559c134c489b2b1432debee6cf3f0
Michael <63615275+micka190@users.noreply.github.com>
2022-12-10 13:17:47 -0700

    Added `InstructionParser` class and tests. Converts an input string into a list of `ICpuInstruction`s.

---
+++ 2022/day 10/Solution/InstructionParser.cs
@@ -0,0 +1,42 @@
+﻿namespace Solution;
+
+public class InstructionParser
+{
+    public List<ICpuInstruction> Parse(string input) =>
+        string.IsNullOrEmpty(input)
+            ? new List<ICpuInstruction>()
+            : input
+                .Trim()
+                .Split('\n')
+                .Select(ParseLine)
+                .ToList();
+
+    private ICpuInstruction ParseLine(string line) =>
+        line switch
+        {
+            "noop" => new NoopInstruction(),
+            _ when line.StartsWith("addx ") => ParseAddToRegisterInstruction(line),
+            _ => throw new ArgumentOutOfRangeException(nameof(line), line, "Unsupported instruction")
+        };
+
+    private RegisterAddInstruction ParseAddToRegisterInstruction(string input)
+    {
+        var segments = input.Split(' ');
+        if (segments.Length != 2)
+        {
+            throw new FormatException($"Expected add instruction to be instruction and value separated by space. Got: \"{input}\"");
+        }
+
+        var registerCharacter = segments[0][^1]; 
+        
+        var register = registerCharacter switch
+        {
+            'x' => Register.X,
+            _ => throw new ArgumentOutOfRangeException(nameof(registerCharacter), registerCharacter, "Unsupported register")
+        };
+
+        var value = int.Parse(segments[1]);
+
+        return new RegisterAddInstruction(register, value);
+    }
+}
---
+++ 2022/day 10/Tests/InstructionParserTests.cs
@@ -0,0 +1,40 @@
+﻿namespace Tests;
+
+public class InstructionParserTests
+{
+    [Fact]
+    public void Parse_ReturnsExpectedInstructions_GivenAdventOfCodeSmallExample()
+    {
+        // Arrange
+        const string input = "noop\n" +
+                             "addx 3\n" +
+                             "addx -5\n";
+        
+        var expectedInstructions = new List<ICpuInstruction>
+        {
+            new NoopInstruction(),
+            new RegisterAddInstruction(Register.X, 3),
+            new RegisterAddInstruction(Register.X, -5),
+        };
+
+        var parser = new InstructionParser();
+
+        // Act
+        var results = parser.Parse(input);
+
+        // Assert
+        results.Should().HaveCount(expectedInstructions.Count);
+        results[0].Should().BeOfType(typeof(NoopInstruction));
+        results[1].Should().BeOfType(typeof(RegisterAddInstruction));
+        results[2].Should().BeOfType(typeof(RegisterAddInstruction));
+
+        for (var i = 1; i < results.Count; ++i)
+        {
+            var addInstruction = results[i] as RegisterAddInstruction;
+            var expectedInstruction = expectedInstructions[i] as RegisterAddInstruction;
+            
+            addInstruction.Value.Should().Be(expectedInstruction.Value);
+            addInstruction.TargetRegister.Should().Be(expectedInstruction.TargetRegister);
+        }
+    }
+}
