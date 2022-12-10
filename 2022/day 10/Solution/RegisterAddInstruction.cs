022ffb56bdca8fe6e49ad53919b66578fabd1ed8
Michael <63615275+micka190@users.noreply.github.com>
2022-12-10 12:51:01 -0700

    Added `RegisterAddInstruction`, which adds a value to a target register after 2 CPU cycles.

---
+++ 2022/day 10/Solution/RegisterAddInstruction.cs
@@ -0,0 +1,23 @@
+﻿namespace Solution;
+
+public class RegisterAddInstruction : ICpuInstruction
+{
+    public bool IsDone { get; private set; }
+
+    public readonly Register TargetRegister;
+    public readonly int Value;
+    
+    public RegisterAddInstruction(Register targetRegister, int value)
+    {
+        TargetRegister = targetRegister;
+        Value = value;
+    }
+
+    public void Perform(Cpu cpu)
+    {
+        cpu.Tick();
+        cpu.Tick();
+        cpu.Registers[TargetRegister] += Value;
+        IsDone = true;
+    }
+}
---
+++ 2022/day 10/Tests/RegisterAddInstructionTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Tests;
+
+public class RegisterAddInstructionTests
+{
+    [Theory]
+    [InlineData(Register.X, 5)]
+    [InlineData(Register.X, -5)]
+    public void Perform_Takes2CyclesToAddToRegister(Register targetRegister, int value)
+    {
+        // Arrange
+        var expectedValue = Cpu.StartingX + value;
+        var cpu = new Cpu();
+        var instruction = new RegisterAddInstruction(targetRegister, value);
+
+        // Act
+        instruction.Perform(cpu);
+
+        // Assert
+        instruction.IsDone.Should().BeTrue();
+        cpu.Cycles[Register.X].Should().HaveCount(2);
+        cpu.Cycles[Register.X][0].Should().Be(Cpu.StartingX);
+        cpu.Cycles[Register.X][1].Should().Be(Cpu.StartingX);
+        cpu.Registers[Register.X].Should().Be(expectedValue);
+    }
+}
