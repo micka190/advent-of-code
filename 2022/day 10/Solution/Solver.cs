e7977b3b233a83b6de37f66a49cbd4b66d49cd96
Michael <63615275+micka190@users.noreply.github.com>
2022-12-10 13:54:04 -0700

    Added `Solver` class and tests. Is able to Solve Part 1. Added Part 1 answer to program output.

---
+++ 2022/day 10/Solution/Solver.cs
@@ -0,0 +1,29 @@
+﻿namespace Solution;
+
+public class Solver
+{
+    private readonly InstructionParser _parser;
+    public Solver(InstructionParser parser) => _parser = parser;
+
+    public int SolveForPartOne(string input)
+    {
+        const int signalStartIndex = 20;
+        const int signalSteps = 40;
+        
+        var instructions = _parser.Parse(input);
+        var cpu = new Cpu();
+        cpu.PerformInstructions(instructions);
+
+        var signalStrength = 0;
+        for (var i = signalStartIndex; i < cpu.Cycles[Register.X].Count; i += signalSteps)
+        {
+            // CPU cycle indexes are 0-based, but the Part 1 signal strength loop is using 1-based indexing.
+            var zeroBasedCycleIndex = i - 1;
+            
+            var cycleStrength = cpu.Cycles[Register.X][zeroBasedCycleIndex] * i;
+            signalStrength += cycleStrength;
+        }
+
+        return signalStrength;
+    }
+}
