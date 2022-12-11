6a6b7426555d1460910683513aa234d5df262b7f
Michael <63615275+micka190@users.noreply.github.com>
2022-12-11 12:43:41 -0700

    Added `KeepAwayGame` class and tests. So far, it properly handles item distribution among monkeys.

---
+++ 2022/day 11/Solution/KeepAwayGame.cs
@@ -0,0 +1,21 @@
+﻿namespace Solution;
+
+public class KeepAwayGame
+{
+    public readonly List<Monkey> Monkeys = new();
+
+    public void DoRound()
+    {
+        foreach (var monkey in Monkeys)
+        {
+            while (monkey.Items.Count > 0)
+            {
+                var item = monkey.Operation.Perform(monkey.Items[0]) / 3;
+                var target = monkey.ItemTest.Perform(item);
+                
+                monkey.Items.RemoveAt(0);
+                Monkeys[target].Items.Add(item);
+            }
+        }
+    }
+}
