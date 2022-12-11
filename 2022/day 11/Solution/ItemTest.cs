e0d276b005a13b2dae32e89a47d8f7e2b1556fdd
Michael <63615275+micka190@users.noreply.github.com>
2022-12-11 12:42:11 -0700

    Updated `ItemTest` to a class that has a `Perform` method to return the target monkey. Added tests.

--- 2022/day 11/Solution/ItemTest.cs
+++ 2022/day 11/Solution/ItemTest.cs
@@ -1,3 +1,20 @@
 ﻿namespace Solution;
 
-public record ItemTest(int Divisor, int TrueTarget, int FalseTarget);
+public class ItemTest
+{
+    public int Divisor { get; set; }
+    public int TrueTarget { get; set; }
+    public int FalseTarget { get; set; }
+    
+    public ItemTest(int divisor, int trueTarget, int falseTarget)
+    {
+        Divisor = divisor;
+        TrueTarget = trueTarget;
+        FalseTarget = falseTarget;
+    }
+
+    public int Perform(int itemWorryLevel) =>
+        itemWorryLevel % Divisor == 0 
+            ? TrueTarget 
+            : FalseTarget;
+}
---
+++ 2022/day 11/Tests/ItemTestTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Tests;
+
+public class ItemTestTests
+{
+    [Fact]
+    public void Perform_ReturnsExpectedTarget_GivenAdventOfCodeExampleInput()
+    {
+        // Arrange
+        var itemTest = new ItemTest(23, 2, 3);
+
+        // Act
+        var trueResult = itemTest.Perform(itemTest.Divisor * 3);
+        var falseResult = itemTest.Perform(itemTest.Divisor + 1);
+
+        // Assert
+        trueResult.Should().Be(itemTest.TrueTarget);
+        falseResult.Should().Be(itemTest.FalseTarget);
+    }
+}
