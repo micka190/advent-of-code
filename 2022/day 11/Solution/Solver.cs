0675e5b91e3f5a0c9ae9cd4d5c64f330a72beba5
Michael <63615275+micka190@users.noreply.github.com>
2022-12-11 12:52:41 -0700

    Added `Solver` class and tests. It produces the correct answer for part 1.

---
+++ 2022/day 11/Solution/Solver.cs
@@ -0,0 +1,30 @@
+﻿namespace Solution;
+
+public class Solver
+{
+    private readonly MonkeyParser _parser;
+
+
+    public Solver(MonkeyParser parser) => _parser = parser;
+
+    public int SolveForPartOne(string input)
+    {
+        const int numberOfRounds = 20;
+        var monkeys = _parser.Parse(input);
+        var game = new KeepAwayGame();
+        game.Monkeys.AddRange(monkeys);
+
+        for (var i = 0; i < numberOfRounds; ++i)
+        {
+            game.DoRound();
+        }
+
+        var topActiveMonkeyCounts = game.Monkeys
+            .OrderByDescending(monkey => monkey.InspectCount)
+            .Take(2)
+            .Select(monkey => monkey.InspectCount)
+            .ToArray();
+
+        return topActiveMonkeyCounts[0] * topActiveMonkeyCounts[1];
+    }
+}
---
+++ 2022/day 11/Tests/SolverTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Tests;
+
+public class SolverTests
+{
+    [Fact]
+    public void SolveForPartOne_ReturnsCorrectAnswer_GivenAdventOfCodeExampleInput()
+    {
+        // Arrange
+        const string input = "Monkey 0:\n" +
+                             "  Starting items: 79, 98\n" +
+                             "  Operation: new = old * 19\n" +
+                             "  Test: divisible by 23\n" +
+                             "    If true: throw to monkey 2\n" +
+                             "    If false: throw to monkey 3\n" +
+                             "\n" +
+                             "Monkey 1:\n" +
+                             "  Starting items: 54, 65, 75, 74\n" +
+                             "  Operation: new = old + 6\n" +
+                             "  Test: divisible by 19\n" +
+                             "    If true: throw to monkey 2\n" +
+                             "    If false: throw to monkey 0\n" +
+                             "\n" +
+                             "Monkey 2:\n" +
+                             "  Starting items: 79, 60, 97\n" +
+                             "  Operation: new = old * old\n" +
+                             "  Test: divisible by 13\n" +
+                             "    If true: throw to monkey 1\n" +
+                             "    If false: throw to monkey 3\n" +
+                             "\n" +
+                             "Monkey 3:\n" +
+                             "  Starting items: 74\n" +
+                             "  Operation: new = old + 3\n" +
+                             "  Test: divisible by 17\n" +
+                             "    If true: throw to monkey 0\n" +
+                             "    If false: throw to monkey 1\n";
+        const int expectedValue = 10605;
+        
+        var parser = new MonkeyParser();
+        var solver = new Solver(parser);
+
+        // Act
+        var result = solver.SolveForPartOne(input);
+
+        // Assert
+        result.Should().Be(expectedValue);
+    }
+}
