6349ab880078b3ab1ca19c090dd2ce1002a8bf7c
Michael <63615275+micka190@users.noreply.github.com>
2022-12-11 12:18:56 -0700

    Added `Operation` class and tests. It parses an item operation string and allows us to perform the operation to get an item's modified worry level.

---
+++ 2022/day 11/Solution/Operation.cs
@@ -0,0 +1,52 @@
+﻿namespace Solution;
+
+public class Operation
+{
+    public string Left { get; }
+    public string Right { get; }
+    public string Operator { get; }
+
+    public Operation(string input)
+    {
+        var segments = input.Split(' ');
+
+        if (segments.Length != 3)
+        {
+            throw new FormatException("Invalid operation input format.");
+        }
+
+        if (segments[1] is not "*" and not "+")
+        {
+            throw new ArgumentException(
+                $"Invalid operator found in input string. Expected \"+\" or \"*\". Got \"{segments[1]}\".",
+                nameof(input)
+            );
+        }
+
+        if (segments[0] != "old" && !int.TryParse(segments[0], out _))
+        {
+            throw new ArgumentException($"Invalid left value in input string. Expected \"old\" or integer. Got \"{segments[0]}\"");
+        }
+        
+        if (segments[2] != "old" && !int.TryParse(segments[2], out _))
+        {
+            throw new ArgumentException($"Invalid right value in input string. Expected \"old\" or integer. Got \"{segments[0]}\"");
+        }
+
+        Left = segments[0];
+        Operator = segments[1];
+        Right = segments[2];
+    }
+
+    public int Perform(int itemWorryLevel)
+    {
+        var left = Left == "old" ? itemWorryLevel : int.Parse(Left);
+        var right = Right == "old" ? itemWorryLevel : int.Parse(Right);
+        return Operator switch
+        {
+            "*" => left * right,
+            "+" => left + right,
+            _ => throw new ArgumentOutOfRangeException(Operator, "Unsupported operator")
+        };
+    }
+}
---
+++ 2022/day 11/Tests/OperationTests.cs
@@ -0,0 +1,51 @@
+﻿namespace Tests;
+
+public class OperationTests
+{
+    [Theory]
+    [InlineData("old + old")]
+    [InlineData("5 + 2")]
+    [InlineData("old + 7")]
+    [InlineData("5 + old")]
+    [InlineData("old * old")]
+    [InlineData("5 * 2")]
+    [InlineData("old * 3")]
+    [InlineData("7 * old")]
+    public void Constructor_ProperlyParsesInputString_GivenValidString(string input)
+    {
+        // Arrange
+        var segments = input.Split(' ');
+        var left = segments[0];
+        var op = segments[1];
+        var right = segments[2];
+
+        // Act
+        var operation = new Operation(input);
+
+        // Assert
+        operation.Left.Should().Be(left);
+        operation.Right.Should().Be(right);
+        operation.Operator.Should().Be(op);
+    }
+    
+    [Theory]
+    [InlineData("old + old", 1, 2)]
+    [InlineData("5 + 2", -1, 7)]
+    [InlineData("old + 7", 3, 10)]
+    [InlineData("5 + old", 3, 8)]
+    [InlineData("old * old", 5, 25)]
+    [InlineData("5 * 2", -1, 10)]
+    [InlineData("old * 3", 8, 24)]
+    [InlineData("7 * old", 3, 21)]
+    public void Perform_ReturnsExpectedValue_GivenValidInput(string input, int itemWorryLevel, int expectedValue)
+    {
+        // Arrange
+        var operation = new Operation(input);
+
+        // Act
+        var result = operation.Perform(itemWorryLevel);
+
+        // Assert
+        result.Should().Be(expectedValue);
+    }
+}
