cffd8b581605b67fcf82b1c9342f2495fedb4c3a
Michael <63615275+micka190@users.noreply.github.com>
2022-12-12 13:04:08 -0700

    Added `BreadthFirstSearch` utility class and tests. Is able to solve AoC example map.

---
+++ 2022/day 12/Solution/BreadthFirstSearch.cs
@@ -0,0 +1,45 @@
+﻿namespace Solution;
+
+public class BreadthFirstSearch
+{
+    public static List<Cell> Search(HeightMap map)
+    {
+        var frontier = new Queue<Cell>();
+        frontier.Enqueue(map.Start);
+
+        var cameFrom = new Dictionary<Cell, Cell?>
+        {
+            [map.Start] = null
+        };
+
+        while (frontier.Count > 0)
+        {
+            var current = frontier.Dequeue();
+            foreach (var next in map.GetNeighbors(current))
+            {
+                if (!cameFrom.ContainsKey(next))
+                {
+                    frontier.Enqueue(next);
+                    cameFrom[next] = current;
+                }
+            }
+        }
+
+        return BuildPath(map, cameFrom);
+    }
+
+    private static List<Cell> BuildPath(HeightMap map, IReadOnlyDictionary<Cell, Cell?> cameFrom)
+    {
+        var pathCell = map.End;
+        var path = new List<Cell>();
+        
+        while (pathCell != map.Start)
+        {
+            path.Add(pathCell);
+            pathCell = cameFrom[pathCell];
+        }
+        path.Reverse();
+
+        return path;
+    }
+}
---
+++ 2022/day 12/Tests/BreadthFirstSearchTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Tests;
+
+public class BreadthFirstSearchTests
+{
+    [Fact]
+    public void Search_ReturnsExpectedPathCount_GivenAdventOfCodeExampleInput()
+    {
+        // Arrange
+        const string representation = "Sabqponm\n" +
+            "abcryxxl\n" +
+            "accszExk\n" +
+            "acctuvwj\n" +
+            "abdefghi\n";
+        const int expectedSteps = 31;
+        var map = new HeightMap(representation);
+
+        // Act
+        var path = BreadthFirstSearch.Search(map);
+
+        // Assert
+        path.Should().HaveCount(expectedSteps);
+    }
+}
