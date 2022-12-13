b8b86732d04092ae4f4c25a1777e8e1edfca01c4
Michael <63615275+micka190@users.noreply.github.com>
2022-12-13 14:22:31 -0700

    Added `PacketComparer` and tests. Able to solve the Part 1 example inputs.

---
+++ 2022/day 13/Solution/PacketComparer.cs
@@ -0,0 +1,64 @@
+﻿namespace Solution;
+
+public class PacketComparer : IComparer<Packet>
+{
+    public const int RightOrder = -1;
+    public const int WrongOrder = 1;
+    public const int TiedOrder = 0;
+    
+    public int Compare(Packet? left, Packet? right)
+    {
+        if (left is null)
+        {
+            throw new ArgumentException("Unexpected comparison target type.", nameof(left));
+        }
+        
+        if (right is null)
+        {
+            throw new ArgumentException("Unexpected comparison target type.", nameof(right));
+        }
+
+        return ComparePackets(left, right);
+    }
+
+    private static int ComparePackets(Packet left, Packet right)
+    {
+        // Both values are numbers, so compare the packets' values.
+        if (left is NumberPacket leftNumber && right is NumberPacket rightNumber)
+        {
+            return leftNumber.Value.CompareTo(rightNumber.Value);
+        }
+        
+        // From this point on, AT LEAST ONE of the two packets is a list.
+        var leftList = ConvertToList(left);
+        var rightList = ConvertToList(right);
+        var lowerBoundLimit = Math.Min(leftList.Values.Length, rightList.Values.Length);
+
+        // Both lists were empty, there's nothing to compare against.
+        if (leftList.Values.Length == rightList.Values.Length && lowerBoundLimit == 0)
+        {
+            return TiedOrder;
+        }
+        
+        // From this point on, both lists have numbers in them, so let's compare each number.
+        for (var i = 0; i < lowerBoundLimit; ++i)
+        {
+            var comparisonResult = ComparePackets(leftList.Values[i], rightList.Values[i]);
+            if (comparisonResult != 0)
+            {
+                return comparisonResult;
+            }
+        }
+
+        // We've reached the end of the shortest list. Comparing both lists' length will return 0 if both had the same length.
+        return leftList.Values.Length.CompareTo(rightList.Values.Length);
+    }
+
+    private static ListPacket ConvertToList(Packet input) =>
+        input switch
+        {
+            ListPacket listPacket => listPacket,
+            NumberPacket numberPacket => new ListPacket(new Packet[] { numberPacket }),
+            _ => throw new ArgumentOutOfRangeException(nameof(input), input, "Unsupported input type")
+        };
+}
---
+++ 2022/day 13/Tests/PacketComparerTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Tests;
+
+public class PacketComparerTests
+{
+    [Theory]
+    [InlineData("[1,1,3,1,1]\n[1,1,5,1,1]", PacketComparer.RightOrder)]
+    [InlineData("[[1],[2,3,4]]\n[[1],4]", PacketComparer.RightOrder)]
+    [InlineData("[9]\n[[8,7,6]]", PacketComparer.WrongOrder)]
+    [InlineData("[[4,4],4,4]\n[[4,4],4,4,4]", PacketComparer.RightOrder)]
+    [InlineData("[7,7,7,7]\n[7,7,7]", PacketComparer.WrongOrder)]
+    [InlineData("[]\n[3]", PacketComparer.RightOrder)]
+    [InlineData("[[[]]]\n[[]]", PacketComparer.WrongOrder)]
+    [InlineData("[1,[2,[3,[4,[5,6,7]]]],8,9]\n[1,[2,[3,[4,[5,6,0]]]],8,9]", PacketComparer.WrongOrder)]
+    public void Compare_ReturnsExpectedAnswer_GivenAdventOfCodeExamplePairs(string packetPair, int expectedComparisonResult)
+    {
+        // Arrange
+        var (left, right) = PacketFactory.FromInput(packetPair)[0];
+        var comparer = new PacketComparer();
+
+        // Act
+        var result = comparer.Compare(left, right);
+
+        // Assert
+        result.Should().Be(expectedComparisonResult);
+    }
+}
