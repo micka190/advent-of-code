c51737fee86970a51dd4e422461722fc34f0433d
Michael <63615275+micka190@users.noreply.github.com>
2022-12-13 14:35:28 -0700

    Added `Solver` class and tests. Is able to get right answer for Part 1. Updated program output to include part 1 solution.

---
+++ 2022/day 13/Solution/Solver.cs
@@ -0,0 +1,14 @@
+﻿namespace Solution;
+
+public class Solver
+{
+    public int SolveForPartOne(string input)
+    {
+        var pairs = PacketFactory.FromInput(input);
+        var comparer = new PacketComparer();
+        return pairs
+            .Select((pair, index) => (Order: comparer.Compare(pair.Left, pair.Right), Index: index + 1))
+            .Where(data => data.Order  == PacketComparer.RightOrder)
+            .Sum(data => data.Index);
+    }
+}
---
+++ 2022/day 13/Tests/SolverTests.cs
@@ -0,0 +1,43 @@
+﻿namespace Tests;
+
+public class SolverTests
+{
+    [Fact]
+    public void SolveForPartOne_ReturnsCorrectAnswer_GivenAdventOfCodeExampleInput()
+    {
+        // Arrange
+        const string input = "[1,1,3,1,1]\n" +
+                "[1,1,5,1,1]\n" +
+                "\n" +
+                "[[1],[2,3,4]]\n" +
+                "[[1],4]\n" +
+                "\n" +
+                "[9]\n" +
+                "[[8,7,6]]\n" +
+                "\n" +
+                "[[4,4],4,4]\n" +
+                "[[4,4],4,4,4]\n" +
+                "\n" +
+                "[7,7,7,7]\n" +
+                "[7,7,7]\n" +
+                "\n" +
+                "[]\n" +
+                "[3]\n" +
+                "\n" +
+                "[[[]]]\n" +
+                "[[]]\n" +
+                "\n" +
+                "[1,[2,[3,[4,[5,6,7]]]],8,9]\n" +
+                "[1,[2,[3,[4,[5,6,0]]]],8,9]\n";
+
+        const int expectedSumOfPairs = 13;
+
+        var solver = new Solver();
+
+        // Act
+        var result = solver.SolveForPartOne(input);
+
+        // Assert
+        result.Should().Be(expectedSumOfPairs);
+    }
+}
