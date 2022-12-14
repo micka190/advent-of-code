f00900c3b7ec7092a991725c446e7d0af96b0c8d
Michael <63615275+micka190@users.noreply.github.com>
2022-12-14 10:51:13 -0700

    Removed whitespace

--- 2022/day 14/Solution/StoneParser.cs
+++ 2022/day 14/Solution/StoneParser.cs
@@ -13,7 +13,6 @@ public class StoneParser
                 .Select(ParseLine)
                 .ToList();
 
-
     private static List<Point> ParseLine(string line) =>
         line
             .Split(" -> ")
