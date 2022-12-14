7160972828341f2ef8424263c927db9971613b23
Michael <63615275+micka190@users.noreply.github.com>
2022-12-14 10:49:41 -0700

    Added `StoneParser` class and tests. Is able to return a list of list of coordinates from a given input.

---
+++ 2022/day 14/Solution/StoneParser.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+namespace Solution;
+
+public class StoneParser
+{
+    public List<List<Point>> Parse(string input) =>
+        string.IsNullOrEmpty(input)
+            ? new List<List<Point>>()
+            : input
+                .Trim()
+                .Split('\n')
+                .Select(ParseLine)
+                .ToList();
+
+
+    private static List<Point> ParseLine(string line) =>
+        line
+            .Split(" -> ")
+            .Select(ParseCoordinate)
+            .ToList();
+
+    private static Point ParseCoordinate(string coordinate)
+    {
+        var segments = coordinate.Split(',');
+        if (segments.Length != 2)
+        {
+            throw new FormatException($"Expected two numbers separated by a comma. Got: \"{coordinate}\"");
+        }
+
+        return new Point(int.Parse(segments[0]), int.Parse(segments[1]));
+    }
+}
---
+++ 2022/day 14/Tests/StoneParserTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace Tests;
+
+public class StoneParserTests
+{
+    [Fact]
+    public void Parse_ReturnsExpectedCoordinates_GivenValidInput()
+    {
+        // Arrange
+        const string input = "0,0 -> 1,1 -> 2,2\n" +
+                             "3,3 -> 4,4 -> 10,10\n" +
+                             "9,3 -> 2,4\n";
+        var parser = new StoneParser();
+
+        // Act
+        var paths = parser.Parse(input);
+
+        // Assert
+        paths.Should().HaveCount(3);
+        
+        paths[0].Should().HaveCount(3);
+        paths[1].Should().HaveCount(3);
+        paths[2].Should().HaveCount(2);
+
+        paths[0].Should().BeEquivalentTo(new[] { new Point(0, 0), new Point(1, 1), new Point(2, 2) });
+        paths[1].Should().BeEquivalentTo(new[] { new Point(3, 3), new Point(4, 4), new Point(10, 10) });
+        paths[2].Should().BeEquivalentTo(new[] { new Point(9, 3), new Point(2, 4) });
+    }
+}
