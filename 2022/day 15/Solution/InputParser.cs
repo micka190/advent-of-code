b309e019991c25b4130fd489a0e8e8c098e47728
Michael <63615275+micka190@users.noreply.github.com>
2022-12-15 12:24:02 -0700

    Added `InputParser` returns sensors and beacons from given advent of code input.

---
+++ 2022/day 15/Solution/InputParser.cs
@@ -0,0 +1,91 @@
+﻿using System.Drawing;
+
+namespace Solution;
+
+public class InputParser
+{
+    private const string SensorPrefix = "Sensor at ";
+    private const string BeaconPrefix = "closest beacon is at ";
+
+    private readonly Dictionary<string, Sensor> _sensors = new(); // Dictionary in case Part 2 introduces duplicate sensor coordinates.
+    private readonly Dictionary<string, Beacon> _beacons = new();
+
+    public (List<Sensor> Sensors, List<Beacon> Beacons) Parse(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return (new List<Sensor>(), new List<Beacon>());
+        }
+
+        _sensors.Clear();
+        _beacons.Clear();
+
+
+        var lines = input.Trim().Split('\n');
+        foreach (var line in lines)
+        {
+            ParseLine(line);
+        }
+
+        return (
+            Sensors: _sensors.Values.ToList(),
+            Beacons: _beacons.Values.ToList()
+        );
+    }
+
+    private void ParseLine(string line)
+    {
+        var segments = line.Split(": ");
+        if (segments.Length != 2)
+        {
+            throw new FormatException($"Expected sensor and beacon coordinates separated by colon. Got: \"{line}\"");
+        }
+
+        var beaconString = segments[1][BeaconPrefix.Length..];
+        var beacon = ParseBeacon(beaconString);
+
+        var sensorString = segments[0][SensorPrefix.Length..];
+        ParseSensor(sensorString, beacon);
+    }
+
+    private Beacon ParseBeacon(string key)
+    {
+        if (!_beacons.ContainsKey(key))
+        {
+            var beacon = new Beacon
+            {
+                Position = ParseCoordinateString(key)
+            };
+            _beacons[key] = beacon;
+        }
+
+        return _beacons[key];
+    }
+
+    private void ParseSensor(string key, Beacon closestBeacon)
+    {
+        if (!_sensors.ContainsKey(key))
+        {
+            var sensor = new Sensor
+            {
+                Position = ParseCoordinateString(key),
+                ClosestBeacon = closestBeacon,
+            };
+            _sensors[key] = sensor;
+        }
+    }
+
+    private static Point ParseCoordinateString(string coordinateString)
+    {
+        var segments = coordinateString.Split(", ");
+        if (segments.Length != 2)
+        {
+            throw new FormatException($"Expected \"x=[int], y=[int]\". Got: \"{coordinateString}\"");
+        }
+
+        var x = int.Parse(segments[0]["x=".Length..]);
+        var y = int.Parse(segments[1]["y=".Length..]);
+
+        return new Point(x, y);
+    }
+}
---
+++ 2022/day 15/Tests/InputParserTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing;
+
+namespace Tests;
+
+public class InputParserTests
+{
+    private Sensor FindSensorAtPosition(List<Sensor> sensors, int x, int y)
+    {
+        var sensor = sensors.Find(s => s.Position.X == x && s.Position.Y == y);
+        if (sensor is null)
+        {
+            throw new ArgumentException("Invalid coordinates were provided.");
+        }
+
+        return sensor;
+    }
+    
+    [Fact]
+    public void Parse_ReturnsListOfSensorsAndBeacons_GivenAdventOfCodeExampleInput()
+    {
+        // Arrange
+        const string input = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15\n" +
+                             "Sensor at x=9, y=16: closest beacon is at x=10, y=16\n" +
+                             "Sensor at x=13, y=2: closest beacon is at x=15, y=3\n" +
+                             "Sensor at x=12, y=14: closest beacon is at x=10, y=16\n" +
+                             "Sensor at x=10, y=20: closest beacon is at x=10, y=16\n" +
+                             "Sensor at x=14, y=17: closest beacon is at x=10, y=16\n" +
+                             "Sensor at x=8, y=7: closest beacon is at x=2, y=10\n" +
+                             "Sensor at x=2, y=0: closest beacon is at x=2, y=10\n" +
+                             "Sensor at x=0, y=11: closest beacon is at x=2, y=10\n" +
+                             "Sensor at x=20, y=14: closest beacon is at x=25, y=17\n" +
+                             "Sensor at x=17, y=20: closest beacon is at x=21, y=22\n" +
+                             "Sensor at x=16, y=7: closest beacon is at x=15, y=3\n" +
+                             "Sensor at x=14, y=3: closest beacon is at x=15, y=3\n" +
+                             "Sensor at x=20, y=1: closest beacon is at x=15, y=3\n";
+
+        const int expectedNumberOfSensors = 14;
+        const int expectedNumberOfBeacons = 6;
+
+        var parser = new InputParser();
+
+        // Act
+        var (sensors, beacons) = parser.Parse(input);
+
+        // Assert
+        sensors.Should().HaveCount(expectedNumberOfSensors);
+        beacons.Should().HaveCount(expectedNumberOfBeacons);
+
+        var firstSensor = FindSensorAtPosition(sensors, 2, 18);
+        firstSensor.Position.Should().BeEquivalentTo(new Point(2, 18));
+        firstSensor.ClosestBeacon.Position.Should().BeEquivalentTo(new Point(-2, 15));
+
+        var firstSharingSensor = FindSensorAtPosition(sensors, 12, 14);
+        var secondSharingSensor = FindSensorAtPosition(sensors, 10, 20);
+        var thirdSharingSensor = FindSensorAtPosition(sensors, 14, 17);
+        new List<Beacon>
+            {
+                firstSharingSensor.ClosestBeacon,
+                secondSharingSensor.ClosestBeacon,
+                thirdSharingSensor.ClosestBeacon
+            }
+            .Should().OnlyContain(beacon => beacon == firstSharingSensor.ClosestBeacon);
+    }
+}
