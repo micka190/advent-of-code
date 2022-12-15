17c08d5dffca00bf52e3749bbcdb40acf02d10e0
Michael <63615275+micka190@users.noreply.github.com>
2022-12-15 12:29:31 -0700

    `Sensor` can now provide the Manhattan distance to its closest beacon through the `DistanceToBeacon` property.

--- 2022/day 15/Solution/Sensor.cs
+++ 2022/day 15/Solution/Sensor.cs
@@ -6,4 +6,8 @@ public class Sensor
 {
     public Point Position { get; set; }
     public Beacon ClosestBeacon { get; set; } = new();
+
+    public int DistanceToBeacon => 
+        Math.Abs(Position.X - ClosestBeacon.Position.X) + 
+        Math.Abs(Position.Y - ClosestBeacon.Position.Y);
 }
---
+++ 2022/day 15/Tests/SensorTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace Tests;
+
+public class SensorTests
+{
+    [Fact]
+    public void DistanceToBeacon_ReturnsExpectedDistance_GivenAdventOfCodeExampleInput()
+    {
+        // Arrange
+        var beacon = new Beacon
+        {
+            Position = new Point(2, 10)
+        };
+
+        var sensor = new Sensor
+        {
+            Position = new Point(8, 7),
+            ClosestBeacon = beacon,
+        };
+
+        const int expectedDistance = 9;
+
+        // Act
+        var distance = sensor.DistanceToBeacon;
+
+        // Assert
+        distance.Should().Be(expectedDistance);
+    }
+}
