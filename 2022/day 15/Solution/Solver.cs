92cc6f2e2349e2701206ad1416ad60f62e2b4b2c
Michael <63615275+micka190@users.noreply.github.com>
2022-12-15 13:07:39 -0700

    Added `Solver` class and tests. It can solve Part 1 given Advent of Code example input. It might not work with the real input, though, since those numbers seem to break the 32-bit int limit. Will probably need to refactor code to use `long` instead of `int`.

---
+++ 2022/day 15/Solution/Solver.cs
@@ -0,0 +1,36 @@
+﻿using System.Drawing;
+
+namespace Solution;
+
+public class Solver
+{
+    private readonly InputParser _parser;
+
+    public Solver(InputParser parser) => _parser = parser;
+
+    public int SolveForPartOne(string input)
+    {
+        const int targetY = 10;
+        
+        var (sensors, beacons) = _parser.Parse(input);
+
+        sensors.Sort((left, right) => left.Position.X - right.Position.X);
+        beacons.Sort((left, right) => left.Position.X - right.Position.X);
+        
+        var rangeStart = Math.Min(sensors[0].Position.X, beacons[0].Position.X);
+        var rangeEnd = Math.Max(sensors[^1].Position.X, beacons[^1].Position.X);
+
+        var invalidPositionCount = 0;
+
+        for (var x = rangeStart; x < rangeEnd; ++x)
+        {
+            var position = new Point(x, targetY);
+            if (sensors.Any(sensor => position != sensor.ClosestBeacon.Position && sensor.DistanceToBeacon >= sensor.DistanceTo(position)))
+            {
+                ++invalidPositionCount;
+            }
+        }
+        
+        return invalidPositionCount;
+    }
+}
---
+++ 2022/day 15/Tests/SolverTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Tests;
+
+public class SolverTests
+{
+    [Fact]
+    public void SolveForPartOne_ReturnsCorrectAnswer_GivenAdventOfCodeExampleInput()
+    {
+        // Arrange
+        const string input = "Sensor at x=2, y=18: closest beacon is at x=-2, y=15\n" +
+                             "Sensor at x=9, y=16: closest beacon is at x=10, y=16\n" +
+                             "Sensor at x=13, y=2: closest beacon is at x=15, y=3\n" +
+                             "Sensor at x=12, y=14: closest beacon is at x=10, y=16\n" +
+                             "Sensor at x=10, y=20: closest beacon is at x=10, y=16\n" +
+                             "Sensor at x=14, y=17: closest beacon is at x=10, y=16\n" +
+                             "Sensor at x=8, y=7: closest beacon is at x=2, y=10\n" +
+                             "Sensor at x=2, y=0: closest beacon is at x=2, y=10\n" +
+                             "Sensor at x=0, y=11: closest beacon is at x=2, y=10\n" +
+                             "Sensor at x=20, y=14: closest beacon is at x=25, y=17\n" +
+                             "Sensor at x=17, y=20: closest beacon is at x=21, y=22\n" +
+                             "Sensor at x=16, y=7: closest beacon is at x=15, y=3\n" +
+                             "Sensor at x=14, y=3: closest beacon is at x=15, y=3\n" +
+                             "Sensor at x=20, y=1: closest beacon is at x=15, y=3\n";
+
+        const int expectedAnswer = 26;
+
+        var parser = new InputParser();
+        var solver = new Solver(parser);
+
+        // Act
+        var result = solver.SolveForPartOne(input);
+
+        // Assert
+        result.Should().Be(expectedAnswer);
+    }
+}
