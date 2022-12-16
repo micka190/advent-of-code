213819e97b13248df142fa876d415d365eb97d0a
Michael <63615275+micka190@users.noreply.github.com>
2022-12-16 14:26:30 -0700

    Added `GraphDistanceMapper` class. It maps the shortest distance between nodes that have values.

---
+++ 2022/day 16/Solution/GraphDistanceMapper.cs
@@ -0,0 +1,88 @@
+﻿namespace Solution;
+
+public class GraphDistanceMapper
+{
+    private readonly Dictionary<string, int> _distances = new();
+
+    public void MapDistanceBetweenValueNodes(Graph graph)
+    {
+        _distances.Clear();
+
+        var nodesToUse = graph.Nodes.Where(node => node.Value > 0 || node == graph.StartingNode).ToList();
+
+        foreach (var start in nodesToUse)
+        {
+            foreach (var end in nodesToUse)
+            {
+                if (start != end)
+                {
+                    var key = GetDistanceKey(start, end);
+                    if (!_distances.ContainsKey(key))
+                    {
+                        _distances[key] = ComputeDistance(start, end);
+                    }
+                }
+            }
+        }
+    }
+
+    private int ComputeDistance(Node start, Node end)
+    {
+        var frontier = new Queue<Node>();
+        frontier.Enqueue(start);
+
+        var cameFrom = new Dictionary<Node, Node?>
+        {
+            [start] = null
+        };
+
+        while (frontier.Count > 0)
+        {
+            var current = frontier.Dequeue();
+            
+            if (current == end)
+            {
+                break;
+            }
+
+            foreach (var next in current.Neighbors)
+            {
+                if (!cameFrom.ContainsKey(next))
+                {
+                    frontier.Enqueue(next);
+                    cameFrom[next] = current;
+                }
+            }
+        }
+
+        var steps = 0;
+
+        var pathNode = end;
+        while (pathNode != start)
+        {
+            ++steps;
+            pathNode = cameFrom[pathNode]!;
+        }
+        
+        return steps;
+    }
+
+    public int GetDistance(Node a, Node b)
+    {
+        var key = GetDistanceKey(a, b);
+
+        if (_distances.ContainsKey(key))
+        {
+            return _distances[key];
+        }
+
+        throw new KeyNotFoundException(
+            $"Distance between Nodes ({a.Id} and {b.Id}) was not found in mapper. Make sure to call Map() method first."
+            );
+    }
+
+    private static string GetDistanceKey(Node a, Node b) =>
+        string.Compare(a.Id, b.Id, StringComparison.Ordinal) == -1
+            ? $"{a.Id}-{b.Id}"
+            : $"{b.Id}-{a.Id}";
+}
---
+++ 2022/day 16/Tests/GraphDistanceMapperTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests;
+
+public class GraphDistanceMapperTests
+{
+    [Fact]
+    public void MapDistanceBetweenValueNodes_ComputesTheDistancesBetweenNodesWithValuesInGraph_GivenGraph()
+    {
+        // Arrange
+        var a = new Node("a", 0);
+        var b = new Node("b", 10);
+        var c = new Node("c", 0);
+        var d = new Node("d", 4);
+
+        a.Connect(new []{b, c});
+        b.Connect(c);
+        c.Connect(d);
+
+        var graph = new Graph(new List<Node> { a, b, c, d }, a);
+        var mapper = new GraphDistanceMapper();
+
+        // Act
+        mapper.MapDistanceBetweenValueNodes(graph);
+
+        // Assert
+        
+        // Immediate neighbors
+        mapper.GetDistance(a, b).Should().Be(1);
+        
+        // Further away
+        mapper.GetDistance(a, d).Should().Be(2); // "a" -> "c" -> "d"
+        mapper.GetDistance(b, d).Should().Be(2); // "b" -> "c" -> "d"
+        mapper.GetDistance(d, b).Should().Be(2); // Same as above but in reverse argument order.
+    }
+}
