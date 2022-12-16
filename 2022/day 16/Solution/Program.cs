7f6c8375eef3d65776b2b06d2a7916cefe4901a2
Michael <63615275+micka190@users.noreply.github.com>
2022-12-16 16:38:32 -0700

    Giving up on Day 16's problem (I've got other things to before the weekend, and it's getting late).
    Brute force attempt for Part 1.
    
    It might work, or maybe not. It solves the example, but takes too long/too much RAM (not sure which comes first) to solve the input I was given (15^15 permutations in the worst case, 11^11 if we remove the valves with flow rates below 10).
    
    Other peoples' solutions seem a lot shorter and smarter. This one completely stumped me.

--- 2022/day 16/Solution/Program.cs
+++ 2022/day 16/Solution/Program.cs
@@ -11,7 +11,10 @@ public static class Program
         else if (File.Exists(args[0]))
         {
             var input = File.ReadAllText(args[0]);
-            // TODO: Solve part 1
+            var parser = new InputParser();
+            var distanceMapper = new GraphDistanceMapper();
+            var solver = new Solver(parser, distanceMapper);
+            Console.WriteLine($"PART 1 - {solver.SolveForPartOne(input)}");
             // TODO: Solve part 2
         }
         else
