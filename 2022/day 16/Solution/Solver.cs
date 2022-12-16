9847fdb1fb115e7fa564297165f8fcdaead2e284
Michael <63615275+micka190@users.noreply.github.com>
2022-12-16 15:22:43 -0700

    Added `Solver` class and tests. It solves Part 1's example properly.

---
+++ 2022/day 16/Solution/Solver.cs
@@ -0,0 +1,101 @@
+﻿namespace Solution;
+
+public class Solver
+{
+    private readonly InputParser _parser;
+    private readonly GraphDistanceMapper _distanceMapper;
+
+    public Solver(InputParser parser, GraphDistanceMapper distanceMapper)
+    {
+        _parser = parser;
+        _distanceMapper = distanceMapper;
+    }
+
+    public int SolveForPartOne(string input)
+    {
+        const int totalMinutes = 30;
+        const int maxMovementMinutes = 28;
+        
+        var nodes = _parser.Parse(input);
+        var graph = new Graph(nodes, nodes[0]);
+
+        _distanceMapper.MapDistanceBetweenValueNodes(graph);
+
+        var nodesWithoutStartingNode = _distanceMapper.NodesUsed.Where(node => node != graph.StartingNode).ToList();
+
+        var permutations = Permute(nodesWithoutStartingNode)
+            .Select(permutation => permutation.Prepend(graph.StartingNode).ToList()).ToList();
+        
+        var scores = new List<int>();
+
+        foreach (var permutation in permutations)
+        {
+            if (DistanceInPermutation(permutation) <= maxMovementMinutes)
+            {
+                scores.Add(GetScore(permutation, totalMinutes));
+            }
+        }
+
+        return scores.Max();
+    }
+
+    private int DistanceInPermutation(List<Node> nodes)
+    {
+        var distance = 0;
+        for (var i = 0; i < nodes.Count - 1; ++i)
+        {
+            var start = nodes[i];
+            var end = nodes[i + 1];
+            distance += _distanceMapper.GetDistance(start, end);
+        }
+
+        return distance;
+    }
+
+    private int GetScore(List<Node> nodes, int minutesRemaining)
+    {
+        const int minutesNeededToOpenValve = 1;
+        
+        var score = 0;
+
+        for (var i = 0; i < nodes.Count - 1 && minutesRemaining > 0; ++i)
+        {
+            var start = nodes[i];
+            var end = nodes[i + 1];
+
+            var distance = _distanceMapper.GetDistance(start, end);
+            minutesRemaining -= distance;
+            minutesRemaining -= minutesNeededToOpenValve;
+            score += minutesRemaining * end.Value;
+        }
+
+        return score;
+    }
+
+    private static IEnumerable<IEnumerable<Node>> Permute(IEnumerable<Node> sequence)
+    {
+        var list = sequence.ToList();
+
+        if (list.Count == 0)
+        {
+            yield return Enumerable.Empty<Node>();
+        }
+        else
+        {
+            var start = 0;
+
+            foreach (var startingElement in list)
+            {
+                var index = start;
+                var remainingItems = list.Where((_, i) => i != index);
+
+                foreach (var permutationOfRemainder in Permute(remainingItems))
+                {
+                    yield return permutationOfRemainder.Prepend(startingElement);
+                }
+
+                start++;
+            }
+        }
+    }
+}
---
+++ 2022/day 16/Tests/SolverTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Tests;
+
+public class SolverTests
+{
+    [Fact]
+    public void SolveForPartOne_ReturnsCorrectAnswer_GivenAdventOfCodeExampleInput()
+    {
+        // Arrange
+        const string input = "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB\n" +
+                             "Valve BB has flow rate=13; tunnels lead to valves CC, AA\n" +
+                             "Valve CC has flow rate=2; tunnels lead to valves DD, BB\n" +
+                             "Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE\n" +
+                             "Valve EE has flow rate=3; tunnels lead to valves FF, DD\n" +
+                             "Valve FF has flow rate=0; tunnels lead to valves EE, GG\n" +
+                             "Valve GG has flow rate=0; tunnels lead to valves FF, HH\n" +
+                             "Valve HH has flow rate=22; tunnel leads to valve GG\n" +
+                             "Valve II has flow rate=0; tunnels lead to valves AA, JJ\n" +
+                             "Valve JJ has flow rate=21; tunnel leads to valve II\n";
+        const int expectedAnswer = 1651;
+
+        var parser = new InputParser();
+        var distanceMapper = new GraphDistanceMapper();
+        var solver = new Solver(parser, distanceMapper);
+
+        // Act
+        var result = solver.SolveForPartOne(input);
+
+        // Assert
+        result.Should().Be(expectedAnswer);
+    }
+}
