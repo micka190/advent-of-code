421f56683594d2973002dd2c653eb8a0f2c8edf7
Michael <63615275+micka190@users.noreply.github.com>
2022-12-16 13:27:08 -0700

    Added `Graph` and tests. `Graphs` are constructed using a list of `Node`s, and can prune the graph of dead leaves using the `Prune` method.

---
+++ 2022/day 16/Solution/Graph.cs
@@ -0,0 +1,31 @@
+﻿namespace Solution;
+
+public class Graph
+{
+    public readonly List<Node> Nodes;
+    public Node StartingNode { get; set; }
+
+    public Graph(IEnumerable<Node> nodes, Node startingNode)
+    {
+        StartingNode = startingNode;
+        Nodes = new List<Node>(nodes);
+    }
+
+    public void Prune()
+    {
+        var leaves = FindEmptyLeaves();
+        while (leaves.Count > 0)
+        {
+            foreach (var leaf in leaves)
+            {
+                // Leaves only have 1 neighbor.
+                leaf.Neighbors.First().Neighbors.Remove(leaf);
+                Nodes.Remove(leaf);
+            }
+
+            leaves = FindEmptyLeaves();
+        }
+    }
+
+    private List<Node> FindEmptyLeaves() => Nodes.Where(node => node is { Value: 0, Neighbors.Count: 1 } && node != StartingNode).ToList();
+}
---
+++ 2022/day 16/Tests/GraphTests.cs
@@ -0,0 +1,34 @@
+﻿namespace Tests;
+
+public class GraphTests
+{
+    [Fact]
+    public void Prune_RemovesLeavesWithNoValues_GivenNodes()
+    {
+        // Arrange
+        var a = new Node("A", 0); // Starting Node is never pruned
+        var b = new Node("B", 0); // Empty leaf
+        var c = new Node("C", 0); // Not a leaf
+        var d = new Node("D", 10); // Non-empty leaf
+        var e = new Node("E", 10); // Becomes non-empty leaf after "F" is removed
+        var f = new Node("F", 0); // Becomes empty leaf once "G" is removed
+        var g = new Node("G", 0); // Empty leaf
+
+        a.Connect(b);
+        a.Connect(c);
+        c.Connect(d);
+        c.Connect(e);
+        e.Connect(f);
+        f.Connect(g);
+
+        var nodes = new List<Node> { a, b, c, d, e, f, g };
+        var graph = new Graph(nodes, a);
+
+        // Act
+        graph.Prune();
+
+        // Assert
+        graph.Nodes.Should().HaveCount(4);
+        graph.Nodes.Should().BeEquivalentTo(new List<Node> { a, c, d, e });
+    }
+}
